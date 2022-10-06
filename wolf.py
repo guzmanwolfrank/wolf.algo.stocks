76363bd9a593b9e1c19f056093d6724cd83b64e5
Wolf <guzmanwolfrank@gmail.com>
2022-10-05 20:40:31 -0400

    Update wolf.py

--- wolf.py
+++ wolf.py
@@ -1,7 +1,6 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt 
-import yahoofinance
 from pylab import *
 import numpy as np
 import scipy.signal as sc
@@ -9,34 +8,37 @@ import datetime as dt
 
 
 #define user input 
-
 user_input = input('Enter Stock Symbol:     ')
-
+#define stock variable as user input
 stock = yf.Ticker(user_input)
 
+#reccomendations defined
 recco = stock.recommendations
 
+#majorholders of stock 
 major = stock.major_holders
 
 allison = [stock, stock.history(period ='5d'), recco]
 
-#new = print(allison)
-
+#set stock period, differs from dataframe time series
 stock.history(period='1mo') 
 
+#defining variable for dataframe using 5day
 eureka = pd.DataFrame(stock.history(period='5d'))
 
+#institutional holders 
 insti = stock.institutional_holders
 
+#frame
 frame = (eureka.columns.values)
 
 leff =list(frame) 
-
 print(leff [0:5])
 
+#define column selection
 N = 5
 
-
+#chart labels
 xlab = 'Intraday Date'
 ylab = 'Price'
 title = (stock)
@@ -46,14 +48,18 @@ plt.xlabel(xlab)
 plt.ylabel(ylab)
 plt.title(title)
 
-
-
-
+#define printout 
 first_n_column = eureka.iloc[:, :N]
 
+#print commands 
 print(first_n_column) 
-
 print(recco)
-
 print(insti) 
 
+#plot chart 
+plt.plot(eureka['Close'])
+
+plt.show()
+
+#clean
+plt.clf()
