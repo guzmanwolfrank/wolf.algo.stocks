7dbe99116e60419492ddde3ba43b7b008f5b02f2
Wolf <guzmanwolfrank@gmail.com>
2022-11-20 16:56:39 -0500

    updated file
    New chart by plotly! 
    --added features: slider, plotly zoom, changed background, candle colors, grid and grid color. 
    --prints out to Firefox effectively

--- wolf.py
+++ wolf.py
@@ -1,63 +1,88 @@
 import yfinance as yf
 import pandas as pd
-import matplotlib.pyplot as plt 
-from pylab import *
-import numpy as np
-import datetime as dt
-
+import plotly.graph_objects as go
 
 #define user input 
-user_input = input('Enter Stock Symbol:     ')
-#define stock variable as user input
+
+user_input = input('Enter Stock Symbol: ')
+
+
+#User Input as stock 
 stock = yf.Ticker(user_input)
 
-#reccomendations defined
-recco = stock.recommendations
 
-#majorholders of stock 
-major = stock.major_holders
+#Yfinance Defs 
+#recco = stock.recommendations
+#major = stock.major_holders
+#insti = stock.institutional_holders
 
+#look back period 
+stock.history(period='6mo') 
+eureka = pd.DataFrame(stock.history(period='90d'))
 
-#set stock period, differs from dataframe time series
-stock.history(period='1mo') 
-
-#defining variable for dataframe using 5day
-eureka = pd.DataFrame(stock.history(period='5d'))
-
-#institutional holders 
-insti = stock.institutional_holders
-
-#frame
 frame = (eureka.columns.values)
 
+#List columns 1-5
 leff =list(frame) 
-print(leff [0:5])
-
-#define column selection
 N = 5
+data = eureka.iloc[:, :N]
+df = data
 
-#chart labels
-xlab = 'Intraday Date'
-ylab = 'Price'
-title = (stock)
+df_ma = df.copy()
 
-#axis labels
-plt.xlabel(xlab)
-plt.ylabel(ylab)
-plt.title(title)
+df_ma.head(10)
 
-#define printout 
-first_n_column = eureka.iloc[:, :N]
+df_ma['MA_10'] = df_ma.Close.rolling(window=10).mean()
 
-#print commands 
-print(first_n_column) 
-print(recco)
-print(insti) 
+df_ma.dropna(inplace=True)
 
-#plot chart 
-plt.plot(eureka['Close'])
+df_plot = df_ma.iloc[-100:].copy()
 
-plt.show()
+df_plot.shape
 
-#clean
-plt.clf()
+
+fig = go.Figure(data=[go.Candlestick(x=df.index,
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+
+fig.add_trace(go.Candlestick(
+    x=df_plot.index, open=df_plot.Open, high=df_plot.High, low=df_plot.Low, close=df_plot.Close,
+    line=dict(width=1), opacity=1,
+    increasing_fillcolor='#24A06B',
+    decreasing_fillcolor="#CC2E3C",
+    increasing_line_color='#2EC886',  
+    decreasing_line_color='#FF3A4C'
+))
+fig.add_trace(go.Scatter(x=df_plot.index, 
+    y=df_plot.MA_10,
+    line=dict(color="#027FC3", width=2),
+    line_shape='spline',
+    name='MA_10'
+    ))
+
+fig.update_layout(width=1000,height=800,
+    margin=dict(l=10,r=60,b=10,t=40),
+    font=dict(size=10,color="#e1e1e1"),
+    paper_bgcolor="#1e1e1e",
+    plot_bgcolor="#1e1e1e",
+    title =str(stock) +"chart",
+    xaxis_title="Time",
+    yaxis_title= "Price")
+
+fig.update_xaxes(
+    gridcolor="#1f292f",
+    showgrid=True,fixedrange=True,rangeslider=dict(visible=False),
+    rangeselector=dict(
+
+)                                                                                   
+)
+
+fig.update_yaxes(
+    gridcolor="#1f292f",
+    showgrid=True
+)
+
+fig.show()
+#fig.write_image('chart.png')
